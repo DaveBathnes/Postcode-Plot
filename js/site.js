ebb294582a3995b13eb0a86ae0d562da799aa01f
Dave <david_rowe@bathnes.gov.uk>
2016-07-03 23:31:50 +0100

    Bug in popup

--- js/site.js
+++ js/site.js
@@ -12,7 +12,8 @@
             key: '1FHNh42TSwPM8rx4MzOlhsFIVo9oGc_fKojqx84ruaMY',
             callback: function (data, tabletop) {
                 $.each(data, function () {
-                    markers.push(L.marker(data.latitude, data.longitude).bindPopup(''));
+                    var popup = '<h4>' + this.Postcode + '</h4><small>' + this.Description + '</small><br/><small>' + this.District + '</small>';
+                    markers.push(L.marker([this.Latitude, this.Longitude]).bindPopup(popup));
                 });
                 markerGroup = L.featureGroup(markers);
                 map.addLayer(markerGroup);
@@ -26,8 +27,16 @@
         var re = RegExp('^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$');
         var OK = re.exec(postcode);
         if (OK) {
-            $.get('https://api.postcodes.io/postcodes/' + postcode, function (data) {
-                window.open('https://docs.google.com/forms/d/1ILPeapyacrJrkSDLS-7aMVHxhM0KaN8w7HT_seLhuPA/viewform?entry.281821379='
+            var data = null;
+            $.ajax({
+                method: 'GET',
+                async: false,
+                url: 'https://api.postcodes.io/postcodes/' + postcode,
+                success: function(response){
+                    data = response;
+                }
+            });
+            window.open('https://docs.google.com/forms/d/1ILPeapyacrJrkSDLS-7aMVHxhM0KaN8w7HT_seLhuPA/viewform?entry.281821379='
                     + postcode + '&entry.2098719225='
                     + (data.result.latitude ? data.result.latitude : '')
                     + '&entry.1579191990='
@@ -38,12 +47,10 @@
                     + (data.result.admin_district ? data.result.admin_district : '')
                     + '&entry.2047396048='
                     + (data.result.admin_ward ? data.result.admin_ward : '')
-                    + '&entry.271850123=')
-            });
+                    + '&entry.271850123=');
+            $('#txtPostcode').val('');
+        } else {
+            alert('Invalid postcode');
         }
     });
-
-
-
-
 });
